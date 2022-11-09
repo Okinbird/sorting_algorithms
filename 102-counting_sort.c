215eeb807434b479335f923b627a9c93c07ff90f
Okinbird <okinyinka@yahoo.co.uk>
2022-11-08 17:22:45 -0800

    counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,66 +2,70 @@
 #include <limits.h>
 #include <stdlib.h>
 
+
 /**
- * get_max - Find max value in array of integers
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
- * @array: array to find max value of
- * @size: size of the array
- * Return: 0
+ * Return: The maximum integer in the array.
  */
 int get_max(int *array, size_t size)
 {
-  int max = INT_MIN;
+int max = INT_MIN;
 
-  while (size--)
-    if (array[size] > max)
-      max = array[size];
+while (size--)
+if (array[size] > max)
+max = array[size];
 
-  return (max);
+return (max);
 }
 
 /**
- * counting_sort - sort an array
- * @array: array to sort
- * @size: size of array to sort
+ * counting_sort - Sort an array of integers in ascending order
+ *                 using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the counting array after setting it up.
  */
 void counting_sort(int *array, size_t size)
 {
-  int *temp, *cpy, j, max;
-  size_t i;
+int *temp, *cpy, j, max;
+size_t i;
 
-  if (!array || size < 2)
-    return;
+if (!array || size < 2)
+return;
 
-  max = get_max(array, size);
-  temp = calloc(max + 1, sizeof(*temp));
-  if (!temp)
-    return;
+max = get_max(array, size);
+temp = calloc(max + 1, sizeof(*temp));
+if (!temp)
+return;
 
-  cpy = malloc(sizeof(*cpy) * size);
-  if (!cpy)
-    {
-      free(temp);
-      return;
-    }
+cpy = malloc(sizeof(*cpy) * size);
+if (!cpy)
+{
+free(temp);
+return;
+}
 
-  for (i = 0; i < size; i++)
-    temp[array[i]]++;
+for (i = 0; i < size; i++)
+temp[array[i]]++;
 
-  for (j = 1; j < max + 1; j++)
-    temp[j] += temp[j - 1];
+for (j = 1; j < max + 1; j++)
+temp[j] += temp[j - 1];
 
-  print_array(temp, max + 1);
+print_array(temp, max + 1);
 
-  for (i = 0; i < size; i++)
-    {
-      temp[array[i]]--;
-      cpy[temp[array[i]]] = array[i];
-    }
+for (i = 0; i < size; i++)
+{
+temp[array[i]]--;
+cpy[temp[array[i]]] = array[i];
+}
 
-  for (i = 0; i < size; i++)
-    array[i] = cpy[i];
+for (i = 0; i < size; i++)
+array[i] = cpy[i];
 
-  free(temp);
-  free(cpy);
+free(temp);
+free(cpy);
 }
